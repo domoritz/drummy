e452f039e30a1db5a98161616eb3bc15b30ef97b
Dominik Moritz <domoritz@gmail.com>
2011-09-16 19:24:59 +0200

    readme for github

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 #include "version.h"
 /*the build git sha has to be refreshed by using the git_sha.py file*/
 const char * build_date = __DATE__ " at " __TIME__;
-const char * build_git_sha = "512e364";
+const char * build_git_sha = "69ffd20";
